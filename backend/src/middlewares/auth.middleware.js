2c58160ca926e248267dcbe88bc56fede0db5e14
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-09-06 21:20:32 +0530

    .get route for /api/food

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const foodPartnerModel = require('../models/fpartner.model')
 const jwt = require('jsonwebtoken');
+const { use } = require('../routes/food.routes');
 
 
 async function authFoodPartnerMiddleware(req, res, next) {
@@ -25,9 +26,30 @@ async function authFoodPartnerMiddleware(req, res, next) {
     }
 }
 
+async function authUserMiddleware(req, res, next) {
+    const token = req.cookies.token;
 
+    if(!token){
+        return res.status(401).json({
+            message: "Please Login First"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findById(decoded.id)
+
+        req.user = user
+
+        next()
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+}
 
 module.exports = {
     authFoodPartnerMiddleware,
-
+    authUserMiddleware
 }
